b0b397a18bb0dbe03d9cf039865f6991d4007150
Nilton Lopes <nilton.lopes@cmtech.com.br>
2021-07-21 22:18:44 -0300

    Curso React

---
+++ meusite/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+/*Para exibir um "Bem vindo" através de components*/
+
+function App(){
+
+    return(
+        <div>
+            Olá mundo
+            
+            <h1>
+                Curso React!
+            </h1>
+        </div>
+    );
+}
+export default App;
